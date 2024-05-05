f08e6e58889480e090999fb61d57111b66294f8b
Gurumoorthi1990 <gurumcsnsk@gmail.com>
2024-05-05 17:42:57 +0530

    complete

---
+++ script.js
@@ -0,0 +1,37 @@
+// const div=document.getElementById("container");
+// div.addEventListener("mouseover",()=>div.style.backgroundColor="red")
+
+
+setTimeout(()=> {
+    document.getElementById("container").innerHTML="<b>10</b>"
+setTimeout(()=> {
+    document.getElementById("container").innerHTML="<b>9</b>"
+setTimeout(()=> {
+    document.getElementById("container").innerHTML="<b>8</b>"
+setTimeout(()=> {
+    document.getElementById("container").innerHTML="<b>7</b>"
+setTimeout(()=> {
+    document.getElementById("container").innerHTML="<b>6</b>"
+setTimeout(()=> {
+    document.getElementById("container").innerHTML="<b>5</b>"
+setTimeout(()=> {
+    document.getElementById("container").innerHTML="<b>4</b>"
+setTimeout(()=> {
+    document.getElementById("container").innerHTML="<b>3</b>"
+setTimeout(()=> {
+    document.getElementById("container").innerHTML="<b>2</b>"
+setTimeout(()=> {
+    document.getElementById("container").innerHTML="<b>1</b>"
+    setTimeout(()=> {
+        document.getElementById("container").innerHTML="<b>Happy Independence day</b>"
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000) 
+},1000)
+},1000)
+},1000)
+},1000)
